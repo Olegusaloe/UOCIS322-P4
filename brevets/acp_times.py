9d2b0ecb4faedefe0c659a915ebec3cf4324cb54
Oleg <olegv@uoregon.edu>
2021-05-18 09:57:56 +0300

    Forgot to update tests

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -13,6 +13,30 @@ import arrow
 #  same arguments.
 #
 
+# ------------------------------------------------------------
+
+def val(arg):
+    
+    v = 200 / 34 
+    if arg == 200:
+       return v
+    
+    v += (100/32)
+    if arg == 300:
+       return v
+
+    v += (100/32)
+    if arg == 400:
+       return v
+
+    v += (200/30)
+    if arg == 600:
+       return v
+
+    v += (400/28)
+    if arg == 1000:
+       return v
+
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
@@ -26,7 +50,74 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    
+    # Cheking for basic errors
+    if (control_dist_km < 0):   
+      raise ValueError  
+
+    if control_dist_km > (brevet_dist_km * 1.2):
+      raise ValueError
+
+    # Determine the number of hours to shift
+    h = 0
+
+    if control_dist_km >= brevet_dist_km:
+       
+       if brevet_dist_km = 200:
+          h = val(200)
+       elif brevet_dist_km = 300:
+          h = val(300)
+       elif brevet_dist_km = 400:
+          h = val(400)
+       elif brevet_dist_km = 600:
+          h = val(600)
+       elif brevet_dist_km = 1000:
+          h = val(1000)
+ 
+    else:  
+      
+      if control_dist_km < 200:
+         h = control_dist_km / 34
+      
+      elif control_dist_km < 300 and control_dist_km >= 200:
+         h = ((control_dist_km - 200) / 32) + val(200)
+      
+      elif control_dist_km < 400 and control_dist_km >= 300:
+         h = ((control_dist_km - 300) / 32) + val(300)       
+      
+      elif control_dist_km < 600 and control_dist_km >= 400:
+         h = ((control_dist_km - 400) / 30) + val(400) 
+      
+      else:
+         h = ((control_dist_km - 600) / 28) + val(600) 
+    
+
+    
+    return brevet_start_time.shift(minutes=round(h * 60))
+
+# ---------------------------------------------------------------
+
+def vall(arg):
+
+    v = 200 / 15 
+    if arg == 200:
+       return v
+    
+    v += (100/15)
+    if arg == 300:
+       return v
+
+    v += (100/15)
+    if arg == 400:
+       return v
+
+    v += (200/15)
+    if arg == 600:
+       return v
+
+    v += (400/11.428)
+    if arg == 1000:
+       return v
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -41,4 +132,54 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+
+    # Cheking for basic errors
+    if (control_dist_km < 0):   
+      raise ValueError  
+
+    if control_dist_km > (brevet_dist_km * 1.2):
+      raise ValueError
+
+    # Determine the number of hours to shift
+    h = 0
+
+    if control_dist_km >= brevet_dist_km:
+       
+       if brevet_dist_km = 200:
+          h = 13.5
+       elif brevet_dist_km = 300:
+          h = 20
+       elif brevet_dist_km = 400:
+          h = 27
+       elif brevet_dist_km = 600:
+          h = 40
+       elif brevet_dist_km = 1000:
+          h = 75
+
+     else:  
+      
+      if control_dist_km < 200:
+         h = control_dist_km / 15
+         
+         if control_dist_km < 60:
+            h += (60 - control_dist_km)/60
+      
+      elif control_dist_km < 300 and control_dist_km >= 200:
+         h = ((control_dist_km - 200) / 15) + vall(200)
+      
+      elif control_dist_km < 400 and control_dist_km >= 300:
+         h = ((control_dist_km - 300) / 15) + vall(300)       
+      
+      elif control_dist_km < 600 and control_dist_km >= 400:
+         h = ((control_dist_km - 400) / 15) + vall(400) 
+      
+      else:
+         h = ((control_dist_km - 600) / 28) + vall(600) 
+
+
+
+    # Shifting
+    return brevet_start_time.shift(minutes=round(h * 60))
+
+
+
--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -1,1 +1,43 @@
-New file
+import nose
+from nose.tools import assert_raises
+
+import arrow
+from brevets import acp_times
+from acp_times import open_time, close_time
+
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s',
+                    level=logging.WARNING)
+
+log = logging.getLogger(__name__)
+
+time = arrow.get("2000-01-01T00:00")
+
+
+def test_neg():
+    assert_raises(ValueError, open_time, -10, 200, time)
+    assert_raises(ValueError, open_time, -20, 300, time)
+    assert_raises(ValueError, open_time, -30, 400, time)
+    assert_raises(ValueError, open_time, -40, 600, time)
+
+
+def test_zero():
+    assert open_time(0, 200, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T00:00'
+    assert open_time(0, 300, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T00:00'
+    assert open_time(0, 400, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T00:00'
+    assert open_time(0, 600, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T00:00'
+    
+    assert close_time(0, 200, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T00:00'
+    assert close_time(0, 300, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T00:00'
+    assert close_time(0, 400, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T00:00'
+    assert close_time(0, 600, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T00:00'
+
+
+def test_200():
+	assert open_time(60, 200, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T01:46'
+    assert close_time(60, 200, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T04:00'
+
+def test_600(): 
+	assert open_time(500, 600, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T15:28'
+    assert close_time(500, 600, time).format('YYYY-MM-DDTHH:mm') == '2000-01-01T09:20'
